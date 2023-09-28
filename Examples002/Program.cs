6559628df465887654103e5996cd18898546614d
Danil <danil.bazarov@inbox.ru>
2023-09-28 17:07:06 +0300

    add Examples002

---
+++ Examples002/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetThirdRank(int n)
+{
+    while (n > 999)
+    {
+        n /= 10;
+    }
+    return n % 10;
+}
+
+bool ValidateNumber(int n)
+{
+    if (n < 100)
+    {
+        Console.WriteLine("Третьей цифры нет!");
+        return false;
+    }
+    return true;
+}
+
+int n = Prompt("Введите число = ");
+if (ValidateNumber(n))
+{
+    Console.WriteLine(GetThirdRank(n));
+}
